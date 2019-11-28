522a854ce01fd6b7504c6b812d6ba0948ccee5af
Joakim Fors <joakim.fors@trivector.se>
2019-11-28 10:47:34 +0100

    Make XML geneartion asynchronous

--- pgr2osm.py
+++ pgr2osm.py
@@ -1,72 +1,83 @@
 import sys
+import io
 import asyncio
 import asyncpg
 import logging
 import postgis
+import lxml.etree as etree
 import xml.etree.ElementTree as ET
 
 from postgis.asyncpg import register
 
 
-def add_node(elem, id, point):
-    node = ET.SubElement(elem, 'node', {
+async def add_node(elem, id, point):
+    node = etree.Element('node', {
         'id': str(id),
         'lat': str(point.y),
         'lon': str(point.x)
     })
+    await elem.write(node)
+    node = None
 
-def add_way(elem, record, line):
-    global node_id
-    way = ET.SubElement(elem, 'way', {'id': str(record['id'])})
-    ET.SubElement(way, 'nd', {'ref': str(record['source'])})
+async def add_way(elem, record, line):
+    way = etree.Element('way', {'id': str(record['id'])})
+    etree.SubElement(way, 'nd', {'ref': str(record['source'])})
     nid = record['id'] * -1000
     for point in line[1:-1]:
-        add_node(elem, nid, point)
-        ET.SubElement(way, 'nd', {'ref': str(nid)})
+        await add_node(elem, nid, point)
+        etree.SubElement(way, 'nd', {'ref': str(nid)})
         nid -= 1
-    ET.SubElement(way, 'nd', {'ref': str(record['target'])})
-    for k, v in {k: v for k, v in record.items() if k not in ['id', 'geom', 'objectid', 'line_geom', 'source', 'target'] and v is not None}.items():
-        ET.SubElement(way, 'tag', {'k': k, 'v': str(v)})
+    etree.SubElement(way, 'nd', {'ref': str(record['target'])})
+    exclude = ['id', 'geom', 'objectid', 'line_geom', 'source', 'target']
+    tags = {k: v for k, v in record.items() if k not in exclude and v is not None}
+    for k, v in tags.items():
+        etree.SubElement(way, 'tag', {'k': k, 'v': str(v)})
+    await elem.write(way)
+    way = None
 
 
 
 async def iterate_vertices(pool, elem):
-    async with pool.acquire() as con:
-        async with con.transaction():
-            async for record in con.cursor('''
-                SELECT id, ST_Transform(the_geom, 4326) as geom
-                FROM nvdb_skane_network_vertices_pgr
-                LIMIT 10
-            '''):
-                add_node(elem, record['id'], record['geom'])
+    async with pool.acquire() as con, con.transaction():
+        async for record in con.cursor('''
+            SELECT id, ST_Transform(the_geom, 4326) as geom
+            FROM nvdb_skane_network_vertices_pgr
+        '''):
+            await add_node(elem, record['id'], record['geom'])
 
 
 async def iterate_edges(pool, elem):
-    async with pool.acquire() as con:
-        async with con.transaction():
-            async for record in con.cursor('''
-                SELECT *, objectid as id, ST_Transform(ST_LineMerge(geom), 4326) as line_geom
-                FROM nvdb_skane_network
-                LIMIT 2
-            '''):
-                add_way(elem, record, record['line_geom'])
+    async with pool.acquire() as con, con.transaction():
+        async for record in con.cursor('''
+            SELECT
+                *,
+                objectid as id,
+                ST_Transform(ST_LineMerge(geom), 4326) as line_geom
+            FROM nvdb_skane_network
+        '''):
+            await add_way(elem, record, record['line_geom'])
 
 
 async def run():
-    #conn = await asyncpg.connect(database='gis')
-    #await register(conn)
-    pool = await asyncpg.create_pool(database='gis', loop=loop, init=register, min_size=5, max_size=10)
+    pool = await asyncpg.create_pool(database='gis', loop=loop, init=register,
+                                     min_size=5, max_size=10)
 
-    #result = await conn.fetch('''SELECT ST_Transform(ST_LineMerge(geom), 4326) as geom FROM nvdb_skane_network LIMIT 2''')
+    async with etree.xmlfile(AsyncXmlOut()) as xf, xf.element('osm', {
+        'version': '0.6', 'upload': 'false', 'generator': 'pgr2osm'
+    }):
+        await asyncio.gather(iterate_vertices(pool, xf),
+                             iterate_edges(pool, xf), return_exceptions=True)
 
-    doc = ET.Element('osm', {'version': '0.6', 'upload': 'false', 'generator': 'pgr2osm'})
-
-    await asyncio.gather(iterate_vertices(pool, doc), iterate_edges(pool, doc), return_exceptions=True)
-
-    ET.dump(doc)
-
-    #print(result)
     await pool.close()
+
+
+class AsyncXmlOut(object):
+    async def write(self, data):
+        print(data.decode('utf-8'))
+
+    async def close(self):
+        pass
+
 
 logging.getLogger("asyncio").setLevel(logging.DEBUG)
 
